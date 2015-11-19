53c48dce852d67867bd931658f6a203fc3fe1299
root <root@beaglebone.localdomain>
2015-11-19 04:12:37 +0000

    added csv file, modified main to output to csv

--- TefnutMain.py
+++ TefnutMain.py
@@ -1,7 +1,20 @@
-import sensors
+import sensors 
+from datetime import datetime
+import csv
 
-htSensor = sensors.HumidTempData('P8_11')
-celcius, farenheight, humidity = htSensor.HTvalues()
+#open the sensor data csv file
+with open('sensors.csv', 'ab') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 
-print '%.4f'%(humidity)
-print '%.2f*C %.2f*F'%(celcius, farenheight)
+    #get the current date and time
+    today = datetime.now()
+
+    #get the humidity/temperature sensor data
+    htSensor = sensors.HumidTempData('P8_11')
+    celcius, farenheight, humidity = htSensor.HTvalues()
+
+    #write date and sensor data to the csv file
+    wr.writerow([today, celcius, farenheight, humidity])
+
+#print '%.4f'%(humidity)
+#print '%.2f*C %.2f*F'%(celcius, farenheight)
