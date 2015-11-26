4cd62ca891a08b36bcada051690c521ce9401be3
root <root@beaglebone.localdomain>
2015-11-26 03:58:57 +0000

    started modifying TefnutMain to write sensor values into database instead of csv file

--- TefnutMain.py
+++ TefnutMain.py
@@ -2,7 +2,31 @@
 
 import sensors 
 from datetime import datetime
+import sqlite3
+
+"""
+#connect to the sensors database
+conn = sqlite3.connect('sensors.db')
+
+today = datetime.now()
+
+#get the humidity/temperature sensor data
+htSensor = sensors.HumidTempData('P8_11')
+celcius, farenheight, humidity = htSensor.HTvalues()
+
+#get moisture sensor data
+moistSensor = sensors.analogSensor('P9_33')
+moistValue = moistSensor.ASoneValue()
+
+#write date and sensor data to the sData table in the sensors database
+conn.execute("INSERT INTO sData( [today, celcius, farenheight, humidity, moistValue])
+
+#close the database connection
+conn.close()
+
+"""
 import csv
+
 
 #open the sensor data csv file
 with open('sensors.csv', 'ab') as myfile:
@@ -22,6 +46,3 @@ with open('sensors.csv', 'ab') as myfile:
 
     #write date and sensor data to the csv file
     wr.writerow([today, celcius, farenheight, humidity, moistValue])
-
-#print '%.4f'%(humidity)
-#print '%.2f*C %.2f*F'%(celcius, farenheight)
